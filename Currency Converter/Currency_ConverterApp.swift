b44aee2133dea18b3bac8c7ce4bd95823791a5d5
Feras Jobeir <fjobeir@gmail.com>
2023-06-27 00:25:51 +0300

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Feras Jobeir on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
