b67d46c3527605217183817cecd3af704591ec0c
Feras Jobeir <fjobeir@gmail.com>
2023-06-27 00:56:53 +0300

    Starting screen

--- Currency Converter/ContentView.swift
+++ Currency Converter/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background").resizable().edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("prancingpony").resizable().scaledToFit()
+                    .frame(height: 200)
+                Text("Currency Exchange").font(.largeTitle)
+                    .foregroundColor(.white)
+                HStack {
+                    VStack {
+                        HStack {
+                            Image("silverpiece").resizable().scaledToFit()
+                                .frame(height: 33)
+                            Text("Silver Piece").font(.headline).foregroundColor(.white)
+                            
+                        }
+                        Text("Text Field")
+                    }
+                    
+                    Image(systemName: "equal")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                    VStack {
+                        HStack {
+                            Text("Gold Piece").font(.headline).foregroundColor(.white)
+                            Image("goldpiece").resizable().scaledToFit()
+                                .frame(height: 33)
+                            
+                            
+                        }
+                        Text("Text Field")
+                    }
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                        .foregroundColor(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
